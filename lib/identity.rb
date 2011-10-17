5b8e97500ee8437818dc8f46e36a680e94e0f8d0
Simen Svale Skogsrud <simen@bengler.no>
2011-10-17 20:43:00 +0200

    minor edits to identity.rb

--- lib/identity.rb
+++ lib/identity.rb
@@ -11,12 +11,15 @@ class Identity < ActiveRecord::Base
     @clients ||= {}
     return @clients[service] if @clients[service]
     account = Account.find_by_identity_id_and_provider(self, service)    
-    fail NotAuthorized.new("Identity #{id} is not authenticated for #{service.capitalize}") unless account.authorized?
+    raise NotAuthorized, "Identity #{id} is not authorized for #{service.capitalize}" unless account.authorized?
     client_class = Object.const_get("#{service}_client".classify) # retrieve the client class, e.g. FacebookClient
     @clients[service] = client_class.new(self, account.credentials)
     @clients[service]
   end
 
+  # Will destroy this identity and all its accounts leaving just a reference
+  # to the succeeding identity in the orphaned_identities-table. Used when
+  # merging identities.
   def orphanize!(new_identity)
     OrphanedIdentity.create!(:old_id => self.id, :identity => new_identity)
     self.destroy
