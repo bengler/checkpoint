d9b5c171871ba0f575722c9954a60f1296fa3ef1
Simen Svale Skogsrud <simen@bengler.no>
2011-10-19 13:48:13 +0200

    authentication can now be completed

--- api/v1.rb
+++ api/v1.rb
@@ -11,14 +11,14 @@ class CheckpointV1 < Sinatra::Base
   helpers do 
     def current_identity
       return Thread.current[:identity] if Thread.current[:identity] 
-      identity_id = SessionManager.identity_id_for_session(cookie[SessionManager.COOKIE_NAME])
+      identity_id = SessionManager.identity_id_for_session(request.cookies[SessionManager::COOKIE_NAME])
       Thread.current[:identity] = Identity.find_by_id(identity_id)
     end
 
     def set_current_identity(identity)
       return if current_identity == identity
-      key = SessionManager.new_session(account.identity.id)
-      set_cookie(SessionManager.COOKIE_NAME, :value => key,
+      key = SessionManager.new_session(identity.id)
+      response.set_cookie(SessionManager::COOKIE_NAME, :value => key,
         :path => '/',
         :expires => Time.now + 1.year)
       Thread.current[:identity] = identity
