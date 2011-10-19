c87c93ab49bf0dfaba731960341ccb5e5838bd49
Simen Svale Skogsrud <simen@bengler.no>
2011-10-19 13:51:12 +0200

    cleaned up root action

--- api/v1.rb
+++ api/v1.rb
@@ -26,12 +26,8 @@ class CheckpointV1 < Sinatra::Base
   end
 
   get '/' do
-    response.set_cookie("test",
-      :value => "Dette er en annen test",
-      :path => "/",
-      :expires => Time.now+1.year)
     <<-HTML
-      <a href='/api/v1/auth/twitter'>Sign in with Twitter</a>
+      <a href='/api/v1/area51/auth/twitter'>Sign in with Twitter</a>
     HTML
   end
 end
