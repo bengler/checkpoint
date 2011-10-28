131a5a6d82ad972048e09fa407c69d523a6d541c
Simen Svale Skogsrud <simen@bengler.no>
2011-10-28 14:35:50 +0200

    added underscore as valid character in domains

--- lib/models/domain.rb
+++ lib/models/domain.rb
@@ -2,6 +2,6 @@ require 'uri'
 
 class Domain < ActiveRecord::Base
   belongs_to :realm
-  validates_format_of :name, :with => /^[a-z0-9\.\-]+$/
+  validates_format_of :name, :with => /^[a-z0-9\.\-\_]+$/
   validates_uniqueness_of :name
 end
