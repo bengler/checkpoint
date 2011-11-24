6fe2ae0bf4e8a7e1bff9ec4b5cf9413997646679
Simen Svale Skogsrud <simen@bengler.no>
2011-11-24 11:00:30 +0100

    Missing identities/me returns 200 {}

--- api/v1/identities.rb
+++ api/v1/identities.rb
@@ -11,15 +11,15 @@ class CheckpointV1 < Sinatra::Base
       render :rabl, :identities, :format => :json
     else
       # Retrieve a single identity
-      @identity = (id == 'me') ? current_identity : Identity.find(id)
-      halt 404, "{}" unless @identity
+      @identity = (id == 'me') ? current_identity : Identity.find_by_id(id)
+      halt 200, "{}" unless @identity
       render :rabl, :identity, :format => :json
     end
   end
 
   get '/identities/:id/accounts/:provider' do |id, provider|
     identity = (id == 'me') ? current_identity : Identity.find(id)
-    halt 500, "No such indentity" unless identity
+    halt 404, "No such indentity" unless identity
     identity == current_identity or check_god_credentials(identity.realm_id)
     @account = identity.accounts.where("provider = ?", params[:provider]).first
     halt 200, "{}" unless @account.try(:authorized?)
