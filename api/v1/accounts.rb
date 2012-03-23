1b6f2d316e30d72e40f775d257234f40c156d73a
Simen Svale Skogsrud <simen@bengler.no>
2012-03-23 15:35:34 +0100

    A possibly temporary admin endpoint to get accounts via provider and uid. Only for gods, mind you

--- api/v1/accounts.rb
+++ api/v1/accounts.rb
@@ -7,10 +7,21 @@ class CheckpointV1 < Sinatra::Base
   # @returns [JSON]
   get '/identities/:id/accounts/:provider' do |id, provider|
     identity = (id == 'me') ? current_identity : Identity.find(id)
-    halt 404, "No such indentity" unless identity
+    halt 404, "No such identity" unless identity
     identity == current_identity or check_god_credentials(identity.realm_id)
     account = identity.accounts.where("provider = ?", params[:provider]).first
     halt 200, "{}" unless account.try(:authorized?)
+    pg :account, :locals => {:account => account}
+  end
+
+
+  # Get an account by provider and uid. Only provided to god-identities.
+  #
+  # @returns [JSON]
+  get '/accounts/:provider/:uid' do |provider, uid|
+    check_god_credentials(current_identity.realm_id)
+    account = Account.where(:realm_id => current_identity.realm_id, :provider => provider, :uid => uid).first
+    halt 404, "No such account" unless account
     pg :account, :locals => {:account => account}
   end
 
