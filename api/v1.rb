d28762d2eb37eb4f855461a93b6fcc5011d5a054
Katrina Owen <katrina.owen@gmail.com>
2012-09-08 14:15:14 +0200

    Delete dead code

--- api/v1.rb
+++ api/v1.rb
@@ -146,26 +146,6 @@ class CheckpointV1 < Sinatra::Base
 
     def log_out
       if (identity = current_session.identity)
-# TODO: Probably no longer necessary
-if false
-        vanilla_keys = identity.realm.keys_for(:vanilla)
-        if vanilla_keys and (vanilla_store = vanilla_keys.store)
-          identity.accounts.where(:provider => 'vanilla').each do |account|
-            if account.token.present?
-              response = Curl::Easy.http_post(
-                "http://#{OmniAuth::Strategies::Vanilla.host}/api/vanilla/v1" \
-                "/#{vanilla_store}/logout/#{account.uid}" \
-                "?oauth_token=#{URI.escape(account.token)}")
-              if (status = response.response_code.to_i) and status != 200
-                LOGGER.error("HTTP error #{status} logging out Vanilla user")
-                if status >= 500
-                  raise "HTTP error #{status} logging out Vanilla user"
-                end
-              end
-            end
-          end
-        end
-end
         unless identity.provisional?
           current_session.identity = nil
         end
