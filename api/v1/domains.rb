44810fa11856396ae69b65b16c73cc89466bc549
Per-Kristian Nordnes <per.kristian.nordnes@gmail.com>
2013-05-15 16:57:01 +0200

    Api methods for manipulating origins for domains

--- api/v1/domains.rb
+++ api/v1/domains.rb
@@ -25,6 +25,25 @@ class CheckpointV1 < Sinatra::Base
   end
 
   # @apidoc
+  # Test if a domain associated with Checkpoint trusts an abritary domain
+  #
+  # @category Checkpoint/Domains
+  # @path /api/checkpoint/v1/domains/:name/allows/:origin
+  # @http GET
+  # @required [String] name The domain name associated with Checkpoint.
+  # @required [String] origin The abritary domain name to test against.
+  # @example /api/checkpoint/v1/domains/acme.org/allows/pinshing.com
+  # @status 404 No associated domain name.
+  # @status 200 [JSON] allowed: true/false
+
+  get '/domains/:name/allows/:origin' do |name, origin|
+    domain = Domain.find_by_name(name)
+    halt 404, "No associated domain name" unless domain
+    content_type :json
+    {allowed: domain.allow_origin?(origin)}.to_json
+  end
+
+  # @apidoc
   # Add a domain to a realm.
   #
   # @description To use checkpoint on a given domain it must be added to a realm first.
@@ -45,6 +64,31 @@ class CheckpointV1 < Sinatra::Base
     domain = Domain.find_by_name(params[:name])
     halt 403, "Domain was connected to realm '#{domain.realm.label}'" if domain && domain.realm != realm
     domain ||= Domain.create!(:name => params[:name], :realm => realm)
+    [201, pg(:domain, :locals => {:domain => domain})]
+  end
+
+  # @apidoc
+  # Add an origin host to a domain.
+  #
+  # @description Add a host to the domain's origins
+  # @note Only gods of the realm may do this.
+  # @category Checkpoint/Domains
+  # @path /api/checkpoint/v1/realms/:label/domains/:name/origins
+  # @http POST
+  # @required [String] label The realm.
+  # @required [String] name The domain name.
+  # @required [String] origin The origin domain name.
+  # @example /api/checkpoint/v1/realms/acme/acme.org/origins
+  # @status 403 The domain is connected to a different realm.
+  # @status 409 You are not a god in this realm.
+  # @status 201 OK
+
+  post '/realms/:label/domains/:name/origins' do |label, name, origin|
+    realm = find_realm_by_label(label)
+    check_god_credentials(realm.id)
+    domain = Domain.find_by_name(name)
+    halt 403, "Domain is connected to realm '#{domain.realm.label}'" if domain && domain.realm != realm
+    domain.add_origin(origin)
     [201, pg(:domain, :locals => {:domain => domain})]
   end
 
@@ -70,4 +114,26 @@ class CheckpointV1 < Sinatra::Base
     halt 204
   end
 
+  # @apidoc
+  # Delete an origin host from a domain.
+  #
+  # @note Only gods of the realm may do this.
+  # @category Checkpoint/Domains
+  # @path /api/checkpoint/v1/realms/:label/domains/:name/origins/:origin
+  # @http DELETE
+  # @required [String] label The realm.
+  # @required [String] name The domain name.
+  # @required [String] origin The origin domain name.
+  # @example /api/checkpoint/v1/realms/acme/domains/acme.org/origins/pinshing.com
+  # @status 403 The domain is connected to a different realm.
+  # @status 409 You are not a god in this realm.
+  # @status 204 Ok.
+
+  delete '/realms/:label/domains/:name/origins/:origin' do |label, name, origin|
+    domain = Domain.find_by_name(name)
+    halt 403, "Domain is connected to '#{domain.realm.label}'" unless domain.realm.label == labels
+    check_god_credentials(domain.realm.id)
+    domain.remove_origin(origin)
+    halt 204
+  end
 end
