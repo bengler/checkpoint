ae6998ad839ecc78d1d81eb5895154cc97fd416d
Lars Haugseth <github@larshaugseth.com>
2013-10-29 09:22:37 +0100

    Shortened code.

--- lib/checkpoint_strategy.rb
+++ lib/checkpoint_strategy.rb
@@ -6,18 +6,9 @@ module Hanuman
     end
 
     def authenticate(username, password)
-      credentials = {
-        :email_or_phone => username,
-        :password       => password,
-      }
-      user = Hanuman.user_service.authentication_by_email_or_phone(credentials)
-
-      account = {
-        :uid   => user.id.to_s,
-        :name  => user.name,
-        :email => user.email
-      }
-      account
+      user = Hanuman.user_service.authentication_by_email_or_phone(
+        email_or_phone: username, password: password)
+      return {uid: user.id.to_s, name: user.name, email: user.email}
     end
 
   end
