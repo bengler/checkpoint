72859cca62369824cbe4a25db47e88d61a296da8
hennikul <henning.kulander@apdm.no>
2013-11-13 17:33:35 +0100

    Fix for checkpoint to complete test migrations during vagrant provision

--- config/strategies.rb
+++ config/strategies.rb
@@ -4,8 +4,26 @@ require "amedia/properties"
 
 environment = ENV['RACK_ENV']
 
-File.open("config/amedia-properties.yml", 'r') do |f|
-  Amedia::Properties::Config.load f, environment
+if environment == 'test'
+  # We don't want to test Gaia's inner workings directly here, so we mock it
+  # This hack should be done in spec_helper, but test environment is also faked
+  # in Rake-tasks for DB:migration.
+  module Gaia
+    class ConfigService
+      def get_property property
+        if property == "hanuman.director.url.prefix"
+          OpenStruct.new({
+                           name: property,
+                           value: "http://example.org/this_should_be_stubbed"
+                         })
+        end
+      end
+    end
+  end
+else 
+  File.open("config/amedia-properties.yml", 'r') do |f|
+    Amedia::Properties::Config.load f, environment
+  end
 end
 
 File.open("config/hanuman.yml", 'r') do |f|
