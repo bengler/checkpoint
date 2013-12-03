962c7544feef284aafcd428e36d0420d19fbd45d
Lars Haugseth <github@larshaugseth.com>
2013-12-03 11:52:35 +0100

    Added date difference check that work in MySQL (and probably only there, sigh.)

--- lib/checkpoint/models/identity.rb
+++ lib/checkpoint/models/identity.rb
@@ -24,7 +24,7 @@ class Identity < ActiveRecord::Base
   }
   scope :anonymous, where("primary_account_id is null")
   scope :not_seen_for_more_than_days, lambda { |days|
-    where("last_seen_on is not null and (current_date - last_seen_on) > ?", days)
+    where("last_seen_on is not null and datediff(current_date, last_seen_on) > ?", days)
   }
 
   def root?
