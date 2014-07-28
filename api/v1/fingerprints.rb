99248a4b2eb0c6d436649392ad1d4f385931421e
Bjørge Næss <bjoerge@gmail.com>
2014-07-28 13:32:28 +0200

    Update docs

--- api/v1/fingerprints.rb
+++ api/v1/fingerprints.rb
@@ -20,11 +20,11 @@ class CheckpointV1 < Sinatra::Base
   # Post a new fingerprint to an identity
   #
   # @category Checkpoint/Fingerprints
-  # @path /api/checkpoint/v1/fingerprints/:fingerprints/identities
-  # @example /api/checkpoint/v1/fingerprints/3eww7uspsy770ohuzoui9wb038up99ci79jz94dpsi6263xi1b/identities
-  # @http GET
-  # @required [String] fingerprints A comma-separated list of fingerprints
-  # @status 200 [JSON] A collection of identities
+  # @path /api/checkpoint/v1/identities/:identity_id/fingerprints
+  # @example /api/checkpoint/v1/identities/1337/fingerprints
+  # @http POST
+  # @required [Array] fingerprints An array of strings to add as fingerprints
+  # @status 200 [JSON] The identity object containing the complete list of fingerprints, including the newly added ones
 
   post '/identities/:identity_id/fingerprints' do |identity_id|
     require_god
