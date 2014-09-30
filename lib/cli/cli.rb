7941d07f26506c89fbc8198eabe94ed5dcd4db47
Thomas Drevon <thomas@bengler.no>
2014-09-30 15:27:06 +0200

    Add after clone task

--- lib/cli/cli.rb
+++ lib/cli/cli.rb
@@ -28,13 +28,50 @@ module Checkpoint
       def seed
         Seed.create_realm('pebbles', :domain => 'pebbles.dev', :keys => {}, :god => true, :title => 'Pebbles Development Realm')
         Seed.create_realm('root', :domain => 'olympus.dev', :keys => {}, :god => true, :title => 'God of all realms')
-
         Seed.list
-
         puts "Remember to put oauth keys in the override.yml file"
+      end
+
+
+      desc "after_clone", "Makeover environment-specific data"
+      def after_clone
+        case ENV['RACK_ENV']
+        when 'production'
+          return
+        when 'development'
+          primary_domain = '.dev'
+        when 'staging'
+          primary_domain = '.staging.o5.no'
+        end
+
+        # rewrite callback urls
+        Callback.all.each do |cb|
+          cb.url = cb.url.sub('.o5.no', primary_domain)
+          cb.save!
+        end
+
+        # add domains
+        ::Realm.all.each do |realm|
+          next if realm.label.include? '_'
+          domain_name = "#{realm.label}#{primary_domain}"
+          create_domain!(domain_name, realm)
+          if ENV['RACK_ENV'] == 'staging'
+            # we need dev domains on staging
+            create_domain!(domain_name.sub('.staging.o5.no', '.dev'), realm)
+          end
+        end
+      end
+
+
+      def create_domain!(domain_name, realm)
+        begin
+          ::Domain.create!(:name => domain_name, :realm => realm)
+          puts "Added domain '#{domain_name}' to realm #{realm.label}"
+        rescue Exception => e
+          puts "Can't create #{domain_name} on #{realm.label}. #{e.message}"
+        end
       end
 
     end
   end
 end
-
