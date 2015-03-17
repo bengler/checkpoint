190ad1c306d70d0d65e068137c99a612e9ba05ca
Alexander Staubo <alex@bengler.no>
2015-03-16 20:01:49 -0400

    Add --primary option to 'bin/checkpoint domain add'.

--- lib/cli/subcommands/domain.rb
+++ lib/cli/subcommands/domain.rb
@@ -3,24 +3,36 @@ module Checkpoint
     class Domain < Thor
 
       desc "add DOMAIN", "Add a domain to realm"
-      method_option :realm, :type => :string, :aliases => "-r", :required => true, :desc => "the realm to which this domain should be added"
-      def add(domain)
+      method_option :realm,
+        type: :string,
+        aliases: "-r",
+        required: true,
+        desc: "the realm to which this domain should be added"
+      method_option :primary,
+        type: :boolean,
+        aliases: "-p",
+        desc: "Set as primary"
+      def add(domain_name)
+        ActiveRecord::Base.transaction do
+          realm = ::Realm.find_by_label(options["realm"])
+          unless realm
+            puts "No such realm: #{options["realm"]}. Create it with \"checkpoint realm create #{options["realm"]}\""
+            return
+          end
 
-        realm = ::Realm.find_by_label(options["realm"])
+          domain = begin
+            ::Domain.create!(name: domain_name, realm: realm)
+          rescue Exception => e
+            abort e.message
+          end
 
-        unless realm
-          puts "No such realm: #{options["realm"]}. Create it with \"checkpoint realm create #{options["realm"]}\""
-          return
+          if options[:primary]
+            realm.primary_domain = domain
+            realm.save!
+          end
+
+          puts "Added domain_name '#{domain.name}' to realm #{realm.label}"
         end
-
-        begin
-          ::Domain.create!(:name => domain, :realm => realm)
-        rescue Exception => e
-          puts "Can't do that. #{e.message}"
-        else
-          puts "Added domain '#{domain}' to realm #{realm.label}"
-        end
-
       end
 
       desc "list", "List domains"
