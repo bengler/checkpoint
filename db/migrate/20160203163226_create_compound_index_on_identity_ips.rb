6ea9e28f5b48695da048d549530ac8a37d15ceaf
Alexander Staubo <alex@bengler.no>
2016-02-03 11:49:06 -0500

    Fix last migration.

--- db/migrate/20160203163226_create_compound_index_on_identity_ips.rb
+++ db/migrate/20160203163226_create_compound_index_on_identity_ips.rb
@@ -1,5 +1,7 @@
 class CreateCompoundIndexOnIdentityIps < ActiveRecord::Migration
 
+  disable_ddl_transaction!
+
   def change
     execute "create index concurrently index_identity_ips_on_address_and_identity_id " \
       "on identity_ips (address, identity_id)"
