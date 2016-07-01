5f1d453fb2b9a7f16030b7f6f9f9328f67248298
Per-Kristian Nordnes <per.kristian.nordnes@gmail.com>
2016-07-01 16:09:01 +0200

    Try to change jwt identity string, seems like ES has problems with :

--- api/v1/jwt.rb
+++ api/v1/jwt.rb
@@ -47,7 +47,7 @@ class CheckpointV1 < Sinatra::Base
 
     def issue_web_token(identity)
       account = identity.primary_account
-      identity_string = "#{account.provider}:#{account.uid}:#{identity.id}"
+      identity_string = "#{account.provider}_#{account.uid}_#{identity.id}"
       claim = {
         i: identity_string,
         x: (Time.now+JWT_TTL).to_i
