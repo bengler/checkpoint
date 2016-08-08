9bbef1199470d2d7eba4a9f9613c7db5c5ae4551
Thomas Drevon <thomas@bengler.no>
2016-08-08 17:30:42 +0200

    Dont output logging to stdout during tests

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -27,7 +27,7 @@ VCR.configure do |c|
   c.filter_sensitive_data("REDACTED:TWITTER_OAUTH_SECRET") { if keys then keys['services']['twitter']['consumer_secret'] end }
 end
 
-ActiveRecord::Base.logger = Logger.new(STDOUT)
+ActiveRecord::Base.logger = Logger.new('/dev/null')
 ActiveRecord::Base.logger.level = Logger::INFO
 
 LOGGER.level = Logger::WARN
