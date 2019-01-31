45acc12b22ca701bae23da22f5a1313055bac0d0
Thomas Drevon <thomas@sanity.io>
2019-01-31 16:08:51 +0100

    Create index concurrently

--- db/migrate/20190131143122_change_fingerprint_index_on_identity.rb
+++ db/migrate/20190131143122_change_fingerprint_index_on_identity.rb
@@ -1,11 +1,11 @@
 class ChangeFingerprintIndexOnIdentity < ActiveRecord::Migration
   def self.up
+    execute "create index concurrently index_fingerprints_on_identities_with_gin on identities using gin(fingerprints)"
     execute "drop index index_fingerprints_on_identities"
-    execute "create index index_fingerprints_on_identities on identities using gin(fingerprints)"
   end
 
   def self.down
-    execute "drop index index_fingerprints_on_identities"
-    execute "create index index_fingerprints_on_identities on identities using gist(fingerprints)"
+    execute "create index concurrently index_fingerprints_on_identities on identities using gist(fingerprints)"
+    execute "drop index index_fingerprints_on_identities_with_gin"
   end
 end
