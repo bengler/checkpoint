6bf39e38b99a33c0d91456e5ff2f1b732008d3df
Thomas Drevon <thomas@sanity.io>
2019-01-31 16:20:50 +0100

    Disable transaction block during migration

--- db/migrate/20190131143122_change_fingerprint_index_on_identity.rb
+++ db/migrate/20190131143122_change_fingerprint_index_on_identity.rb
@@ -1,4 +1,5 @@
 class ChangeFingerprintIndexOnIdentity < ActiveRecord::Migration
+  disable_ddl_transaction!
   def self.up
     execute "create index concurrently index_fingerprints_on_identities_with_gin on identities using gin(fingerprints)"
     execute "drop index index_fingerprints_on_identities"
